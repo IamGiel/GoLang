501c5443bf56e60684535ea3433f03e29e815a2d
Server <november14giel@gmail.com>
2018-06-03 00:02:07 -0400

    := syntax for var

--- hello.go
+++ hello.go
@@ -7,13 +7,25 @@ import (
 func main(){
 	// fmt.Println("Hello 🌎");
 
+	//===============
+
 	// var x int
 	// fmt.Println(x) // output 0
 
-	var x int = 7
-	var y int = 5
-	var sum int = x + y 
+	//===============
 
-	fmt.Println(sum)
+	// var x int = 7
+	// var y int = 5
+	// var sum int = x + y
+
+	// fmt.Println(sum) // output 12
+
+	//===============
+	// x := 7 
+	// y := 5 
+	// var sum int = x + y
+
+	// fmt.Println(sum) // output 12
+
 
 }
