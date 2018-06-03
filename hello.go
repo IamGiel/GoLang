fd6c01f2a0772b458558b1137a23c8c954409ce3
Server <november14giel@gmail.com>
2018-06-02 23:53:55 -0400

    variables and func

---
+++ hello.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main(){
+	// fmt.Println("Hello 🌎");
+
+	// var x int
+	// fmt.Println(x) // output 0
+
+	var x int = 7
+	var y int = 5
+	var sum int = x + y 
+
+	fmt.Println(sum)
+
+}
